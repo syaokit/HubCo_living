6c65082a2b39bb029d348855fa4e37be2699c6c1
syaokit <57128444+syaokit@users.noreply.github.com>
2020-10-12 13:15:31 +0800

    Initial commit

---
+++ HubCo_living/custRegister.aspx
@@ -0,0 +1,91 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="custRegister.aspx.cs" Inherits="HubCo_living.custRegister" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style> 
+        table tr td{
+
+            border:2px black solid;
+        }
+         
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>Customer Register</h1>
+         
+       <asp:Panel runat="server" ID="panel">
+        <table>
+
+            <tr>
+                <td>Email</td>
+                <td>
+                    <asp:TextBox ID="emailTxt" runat="server" TextMode="Email"></asp:TextBox>
+                </td>
+            </tr>
+
+              <tr>
+                <td>Full Name</td>
+                <td>
+                    <asp:TextBox ID="fullNameTxt" runat="server" ></asp:TextBox>
+                </td>
+            </tr>
+
+              <tr>
+                <td>Contact No</td>
+                <td>
+                    <asp:TextBox ID="contactNoTxt" runat="server" ></asp:TextBox>
+                </td>
+            </tr>
+
+              <tr>
+                <td>Gender</td>
+                <td>
+         <asp:RadioButton runat="server" Text="Male" GroupName="gender"   ID ="gender1"></asp:RadioButton>
+         <asp:RadioButton runat="server" Text="Female" GroupName="gender" ID="gender2"></asp:RadioButton> 
+                </td>
+            </tr>
+
+              <tr>
+                <td>IC Number</td>
+                <td>
+                    <asp:TextBox ID="icNumberTxt" runat="server" ></asp:TextBox>
+                </td>
+            </tr>
+            
+            <tr>
+                <td>D.O.B</td>
+                <td>
+                    <asp:TextBox ID="dobTxt" runat="server" TextMode="Date"></asp:TextBox>
+                     
+                </td>
+            </tr>
+
+            <tr>
+                <td>Password</td>
+                <td>
+                    <asp:TextBox ID="passwordTxt" runat="server" TextMode="Password"></asp:TextBox>
+                </td>
+            </tr>
+
+              <tr>
+                <td>Re-Enter Password</td>
+                <td>
+                    <asp:TextBox ID="rePasswordTxt" runat="server" TextMode="Password"></asp:TextBox>
+                </td>
+            </tr>
+        </table>
+           </asp:Panel>
+        <asp:Button ID="registerBtn" runat="server" Text="Register" OnClick="registerBtn_Click"/>
+        <asp:Button ID="clearBtn" runat="server" Text="Clear"  OnClick="clearBtn_Click"/>
+
+
+          
+        <br />
+        <asp:Label ID="errList" runat="server" Text="error"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ HubCo_living/custRegister.aspx.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HubCo_living
+{
+    public partial class custRegister : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+        protected void clearBtn_Click(object sender, EventArgs e)
+        {
+            emailTxt.Text = "";
+            fullNameTxt.Text = "";
+            contactNoTxt.Text = "";
+            icNumberTxt.Text = "";
+            passwordTxt.Text = "";
+            rePasswordTxt.Text = "";
+            dobTxt.Text = "";
+           gender1.Checked = false;
+            gender2.Checked = false;
+
+        }
+
+        protected void registerBtn_Click(object sender, EventArgs e)
+        {
+            
+            string pass1 = passwordTxt.Text;
+            string pass2 = rePasswordTxt.Text;
+            string email = emailTxt.Text;
+            string fullname = fullNameTxt.Text;
+            string contactNo = contactNoTxt.Text;
+            string icNumber = icNumberTxt.Text;
+            string gender = "";
+            DateTime dob = DateTime.Parse( dobTxt.Text.ToString());
+
+
+            if (gender1.Checked == true)
+            {
+                gender = "male";
+            }
+            else
+            {
+                gender = "female";
+            }
+
+            String validInfo = validateInfo(contactNo, icNumber, dob);
+            String validPass = validatePassword(pass1, pass2);
+            Boolean validEmail = validateEmail(email);
+
+
+            errList.Text = "";
+            String errMsg = "";
+
+            if (validEmail == true && validPass.Equals("") && validInfo.Equals(""))
+            {
+                SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|db.mdf;Integrated Security=True;");
+                SqlCommand cmd = new SqlCommand("insert into [Customers] values (@fullName,@email,  @contactNo, @gender, @icNumber, @dob,@password )", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@email", email);
+                cmd.Parameters.AddWithValue("@fullName", fullname);
+                cmd.Parameters.AddWithValue("@contactNo", contactNo);
+                cmd.Parameters.AddWithValue("@gender", gender);
+                cmd.Parameters.AddWithValue("@icNumber", icNumber);
+                cmd.Parameters.AddWithValue("@dob", dob);
+                cmd.Parameters.AddWithValue("@password", pass1);
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                emailTxt.Text = "";
+                fullNameTxt.Text = "";
+                contactNoTxt.Text = "";
+                icNumberTxt.Text = "";
+                passwordTxt.Text = "";
+                rePasswordTxt.Text = "";
+                dobTxt.Text = "";
+                gender1.Checked = false;
+                gender2.Checked = false;
+                errList.Text = "Account has been created successful";
+                errList.Style["color"] = "green";
+            }
+            else
+            {
+                errMsg += validPass;
+                errMsg += validInfo;
+
+                if (validEmail != true)
+                    errMsg += "Email is already exist<br/>";
+
+                errList.Text = errMsg;
+                errList.Style["color"] = "red";
+
+            }
+        }
+
+
+
+
+        private String validatePassword(string password, string password2)
+        {
+            String msg = "";
+            Boolean valid1 = false;
+            Boolean valid2 = false;
+
+            //check diff
+            if (password.Equals(password2))
+            {
+
+                //check length
+                if (password.Length >= 8)
+                {
+                    //check contain digit
+                    for (int i = 0; i < password.Length; i++)
+                    {
+                        char c = password[i];
+                        if (Char.IsDigit(c))
+                        {
+                            valid1 = true;
+                            break;
+                        }
+
+                    }
+
+                    //check contain letter
+                    for (int i = 0; i < password.Length; i++)
+                    {
+                        char c = password[i];
+                        if (Char.IsLetter(c))
+                        {
+                            valid2 = true;
+                            break;
+                        }
+
+                    }
+
+                    if (valid1 == false || valid2 == false)
+                        msg += "Password must contains alphanumeric <br/>";
+                }
+                else
+                {
+                    msg += "Password length must more than 8 <br/>";
+                } 
+            }
+            else
+            {
+                msg += "Password and Confirm Password are different <br/>";
+            } 
+            return msg;
+        }
+
+        private Boolean validateEmail(string email)
+        {
+            Boolean valid = true;
+
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|db.mdf;Integrated Security=True;");
+            SqlCommand cmd = new SqlCommand("select email from [Customers] where email = @email ", con);
+            con.Open();
+            cmd.Parameters.AddWithValue("@email", email);
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                valid = false;
+            }
+            con.Close();
+
+            return valid;
+        }
+
+        private String validateInfo(string contactNo, string icNumber, DateTime dob)
+        {
+            String errMsg = "";
+
+            //check contact Number
+            Boolean validContactNo = true;
+
+            if (contactNo.Length != 10)
+            {
+                validContactNo = false; 
+            }
+            for(int i=0; i < contactNo.Length; i++)
+            {
+                char number = contactNo[i];
+                if (!(Char.IsDigit(number)))
+                {
+                    validContactNo = false;
+                }
+            }
+
+            if(validContactNo==false)
+                errMsg += "Contact Number should be 10 digits number..<br/>";
+
+            //check ic Number
+            Boolean validIcNo = true;
+
+            if (icNumber.Length != 12)
+            {
+                validIcNo = false;
+            }
+            for (int i = 0; i < icNumber.Length; i++)
+            {
+                char number = icNumber[i];
+                if (!(Char.IsDigit(number)))
+                {
+                    validIcNo = false;
+                }
+            }
+
+            if (validIcNo == false)
+                errMsg += "IC  Number should be 12 digits number..<br/>";
+
+            //check dob 
+            DateTime now = DateTime.Now;
+            if(now.CompareTo(dob) == -1)
+            {
+                errMsg += "D.O.B should be earlier than today date..<br/>";
+            }
+
+            return errMsg;
+        }
+    }
+}
